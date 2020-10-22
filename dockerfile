48aebf6c112949b5602813c577f2b4ec3119d4cc
PGOECONTB008 <pgoecontb008@PGOECONTB008.mne.intranet>
2020-10-21 22:51:21 -0400

    update proxy

--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
-FROM node:12.19.0
+FROM node:12-alpine
 
 WORKDIR /src/app
 
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello Word'));
+app.get('/', (req, res) => res.send('Gerson Online'));
 
 app.listen(3000);
 
